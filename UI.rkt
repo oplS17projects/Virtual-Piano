8982dccbf84b78cf07f1b9165dfa0af44a86d154
MInh Nguyen <minhngu96@gmail.com>
2017-04-07 22:43:49 -0400

    Added keyboard alignment

--- UI.rkt
+++ UI.rkt
@@ -1,8 +1,8 @@
 #lang racket
 (require picturing-programs)
-
 (define A (rectangle 80 300 "outline" "black"))
-(define A1 (rectangle 80 300 "solid" "black"))
+(define A1 (rectangle 80 300 "solid" "red"))
+(define new (rectangle 50 200 "solid" "red"))
 (define B (rectangle 80 300 "outline" "black"))
 (define C (rectangle 80 300 "outline" "black"))
 (define D (rectangle 80 300 "outline" "black"))
@@ -16,20 +16,46 @@
 (define L (rectangle 50 200 "solid" "black"))
 (define M (rectangle 50 200 "solid" "black"))
 
-(define white-keys
-  (beside A B C D E F G H))
-
-(define black-keys
-  (overlay/xy  I 100 0 (overlay/xy J 140 0 (overlay/xy K 80 0
-     (overlay/xy L 80 0 M)))))
-
 (define PIANO
-  (overlay/align/offset "left" "top" black-keys -50 0 white-keys))
+  (overlay/align/offset "left" "top" (overlay/xy  I 100 0 (overlay/xy J 140 0 (overlay/xy K 80 0
+     (overlay/xy L 80 0 M)))) -50 0 (beside A B C D E F G H)))
+
+(define (display t)
+  (place-image/align
+   PIANO
+   100 0
+   "left" "top"
+   (empty-scene 800 600)))
 
 (define (key-press im k)
-   (cond [(equal? k "a") (set! A A1)]
+   (cond [(equal? k "a") (set! PIANO (overlay/align/offset "left" "top" (overlay/xy  I 100 0 (overlay/xy J 140 0 (overlay/xy K 80 0
+     (overlay/xy L 80 0 M)))) -50 0 (beside A1 B C D E F G H)))]
+         [(equal? k "s") (set! PIANO (overlay/align/offset "left" "top" (overlay/xy  I 100 0 (overlay/xy J 140 0 (overlay/xy K 80 0
+     (overlay/xy L 80 0 M)))) -50 0 (beside A A1 C D E F G H)))]
+         [(equal? k "d") (set! PIANO (overlay/align/offset "left" "top" (overlay/xy  I 100 0 (overlay/xy J 140 0 (overlay/xy K 80 0
+     (overlay/xy L 80 0 M)))) -50 0 (beside A B A1 D E F G H)))]
+         [(equal? k "f") (set! PIANO (overlay/align/offset "left" "top" (overlay/xy  I 100 0 (overlay/xy J 140 0 (overlay/xy K 80 0
+     (overlay/xy L 80 0 M)))) -50 0 (beside A B C A1 E F G H)))]
+         [(equal? k "g") (set! PIANO (overlay/align/offset "left" "top" (overlay/xy  I 100 0 (overlay/xy J 140 0 (overlay/xy K 80 0
+     (overlay/xy L 80 0 M)))) -50 0 (beside A B C D A1 F G H)))]
+         [(equal? k "h") (set! PIANO (overlay/align/offset "left" "top" (overlay/xy  I 100 0 (overlay/xy J 140 0 (overlay/xy K 80 0
+     (overlay/xy L 80 0 M)))) -50 0 (beside A B C D E A1 G H)))]
+         [(equal? k "j") (set! PIANO (overlay/align/offset "left" "top" (overlay/xy  I 100 0 (overlay/xy J 140 0 (overlay/xy K 80 0
+     (overlay/xy L 80 0 M)))) -50 0 (beside A A C D E F A1 H)))]
+         [(equal? k "k") (set! PIANO (overlay/align/offset "left" "top" (overlay/xy  I 100 0 (overlay/xy J 140 0 (overlay/xy K 80 0
+     (overlay/xy L 80 0 M)))) -50 0 (beside A B C D E F G A1)))]
+         [(equal? k "w") (set! PIANO (overlay/align/offset "left" "top" (overlay/xy  new 100 0 (overlay/xy J 140 0 (overlay/xy K 80 0
+     (overlay/xy L 80 0 M)))) -50 0 (beside A B C D E F G H)))]
+         [(equal? k "e") (set! PIANO (overlay/align/offset "left" "top" (overlay/xy  I 100 0 (overlay/xy new 140 0 (overlay/xy K 80 0
+     (overlay/xy L 80 0 M)))) -50 0 (beside A B C D E F G H)))]
+         [(equal? k "t") (set! PIANO (overlay/align/offset "left" "top" (overlay/xy  I 100 0 (overlay/xy J 140 0 (overlay/xy new 80 0
+     (overlay/xy L 80 0 M)))) -50 0 (beside A B C D E F G H)))]
+         [(equal? k "y") (set! PIANO (overlay/align/offset "left" "top" (overlay/xy  I 100 0 (overlay/xy J 140 0 (overlay/xy K 80 0
+     (overlay/xy new 80 0 M)))) -50 0 (beside A B C D E F G H)))]
+         [(equal? k "u") (set! PIANO (overlay/align/offset "left" "top" (overlay/xy  I 100 0 (overlay/xy J 140 0 (overlay/xy K 80 0
+     (overlay/xy L 80 0 new)))) -50 0 (beside A B C D E F G H)))]
         [else im]))
 
-(big-bang PIANO
-          (to-draw show-it)
+(big-bang 0
+          (to-draw display)
           (on-key key-press))
